998ef15ae3c82c65921b358b423d48f2e48b6b8a
Rudramadhaba mishra <76451927+RUDRAMADHABA@users.noreply.github.com>
2021-12-20 18:18:54 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,34 @@
 // var a=0;
 function sv(){
-  a=a+1;
+  // a=a+1;
   var name=document.querySelector('#name').value;
   var email=document.querySelector('#email').value;
 
-  firebase.database().ref('val/').set({
-      cpp:`${a}`,
-  });
-  firebase.database().ref(`user${a}/`).set({
+ // firebase.database().ref('val/').set({
+   //   cpp:`${a}`,
+ // });
+  firebase.database().ref().child("email/").push({
     name : `${name}`,
     email: `${email}`,
-  });
-console.log(a);
-location.reload();
+    temail:"0",
+    cemail:"1"
+}).then(()=>{
+    console.log("lol")
+    location.reload();
 alert('Data saved!');
 }
-function updateinfo(){
-var starCountRef = firebase.database().ref('val/');
-starCountRef.on('value', (snapshot) => {
-const data = snapshot.val();
-a=parseInt(data.cpp);
-console.log(a);
-
 });
+//console.log(a);
+
+
 }
-updateinfo();
+//function updateinfo(){
+//var starCountRef = firebase.database().ref('val/');
+//starCountRef.on('value', (snapshot) => {
+//const data = snapshot.val();
+//a=parseInt(data.cpp);
+//console.log(a);
+//
+//});
+//}
+//updateinfo();
